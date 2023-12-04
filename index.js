7b34b75b857c91d662b0593afc4bc83fc75d3cbf
Washington <washington195luiz@gmail.com>
2023-12-04 01:09:01 -0300

    new test 2

--- index.js
+++ index.js
@@ -25,97 +25,99 @@ app.use(helmet({
         }
     }
 }));
-
+let connected = false;
+let initialized = false;
+let destroySession = false;
+let svgToSend = '';
 const server = require('http').createServer(app);
 const wss = new WebSocket.Server({ server: server })
+wss.on('connection', function connection(ws) {
+    if(svgToSend !== ''){
+        ws.send(svgToSend)
+    }
+});
+wss.broadcast = function (data) {
+    wss.clients.forEach(client => client.send(data));
+};
+; (async function connection() {
+    try {
+        let client = new Client({
+            authStrategy: new LocalAuth({ clientId: "WHATSAPP-PDF" })
+            //authStrategy: new LocalAuth({ clientId: "WHATSAPP-PDF-Developer" })
+        })
+        if (initialized == false) {
+            client.initialize();
+            initialized = true;
+        }
 
-    ; (async function connection() {
-        let connected = false;
-        let destroySession = false;
-        wss.on('connection', function connection(ws) {
+        client.on('loading_screen', (percent, message) => {
+            svgToSend = '';
+            wss.broadcast("connecting")
 
-            try {
-                let client = new Client({
-                    authStrategy: new LocalAuth({ clientId: "WHATSAPP-PDF" })
-                    //authStrategy: new LocalAuth({ clientId: "WHATSAPP-PDF-Developer" })
-                })
-                client.initialize();
+            console.log('LOADING SCREEN', percent, message);
+        })
 
-                client.on('loading_screen', (percent, message) => {
-                    ws.send("connecting")
-                    console.log('LOADING SCREEN', percent, message);
-                })
+        client.on('authenticated', () => {
+            console.log('AUTHENTICATED')
+        })
 
-                client.on('authenticated', () => {
-                    console.log('AUTHENTICATED')
-                })
+        client.on('auth_failure', msg => {
+            console.log("AUTHENTICATION FAILURE", msg);
+        })
 
-                client.on('auth_failure', msg => {
-                    console.log("AUTHENTICATION FAILURE", msg);
-                })
-
-                app.get('/connected', (req, res) => {
-                    if (connected) {
-                        res.status(200).json({ response: true })
-                    } else {
-                        res.status(200).json({ response: false })
-                    }
-                })
-
-                app.get('/qrcode', (req, res) => {
-                    console.log('get qrcode')
-                    if (destroySession) {
-                        client.initialize();
-                    }
-                    client.on('qr', async qr => {
-                        try {
-                            console.log("Trying generate new qr code")
-                            var svg_string = qrImage.imageSync(qr, { type: 'svg' });
-                            res.status(200).json({ response: svg_string })
-                            return
-                        } catch (e) {
-                            console.log(e);
-                        }
-                    });
-                })
-
-
-
-                client.on('ready', async () => {
-                    console.log('Client is ready!');
-                    ws.send("ready")
-                    client.sendMessage(client.info.wid._serialized, 'Conectado!')
-                    connected = true;
-                })
-                client.on('disconnected', async () => {
-                    console.log('Disconnected!');
-                    connected = false;
-                    destroySession = true;
-
-                })
-
-                app.post("/sendMessage", upload.array("files"), async (req, res) => {
-                    fs.renameSync(`${req.files[0].destination}${req.files[0].filename}`, `${req.files[0].destination}${req.files[0].originalname}`);
-                    const media = MessageMedia.fromFilePath(`${req.files[0].destination}${req.files[0].originalname}`);
-                    let number = req.body.number
-                    let numberId = await client.getNumberId('55' + number);
-                    if (numberId == null) {
-                        res.status(400).json({ error: "Este não é um whatsapp válido.\nVerifique o número e tente novamente!" })
-                        return
-                    }
-                    client.sendMessage(numberId._serialized, media);
-                    fs.rmSync(`${req.files[0].destination}${req.files[0].originalname}`);
-                    res.status(200).json({ message: "Arquivo Enviado!" })
-                    return
-                })
-
-
-            } catch (err) {
-                console.log(err);
+        app.get('/connected', (req, res) => {
+            if (connected) {
+                res.status(200).json({ response: true })
+            } else {
+                res.status(200).json({ response: false })
             }
+        })
 
+        client.on('qr', async qr => {
+            try {
+                console.log("Trying generate new qr code")
+                var svg_string = qrImage.imageSync(qr, { type: 'svg' });
+                svgToSend = svg_string;
+                wss.broadcast(svg_string);
+            } catch (e) {
+                console.log(e);
+            }
         });
-    })()
+
+
+        client.on('ready', async () => {
+            console.log('Client is ready!');
+            wss.broadcast("ready")
+            client.sendMessage(client.info.wid._serialized, 'Conectado!')
+            connected = true;
+        })
+        client.on('disconnected', async () => {
+            console.log('Disconnected!');
+            connected = false;
+            destroySession = true;
+
+        })
+
+        app.post("/sendMessage", upload.array("files"), async (req, res) => {
+            fs.renameSync(`${req.files[0].destination}${req.files[0].filename}`, `${req.files[0].destination}${req.files[0].originalname}`);
+            const media = MessageMedia.fromFilePath(`${req.files[0].destination}${req.files[0].originalname}`);
+            let number = req.body.number
+            let numberId = await client.getNumberId('55' + number);
+            if (numberId == null) {
+                res.status(400).json({ error: "Este não é um whatsapp válido.\nVerifique o número e tente novamente!" })
+                return
+            }
+            client.sendMessage(numberId._serialized, media);
+            fs.rmSync(`${req.files[0].destination}${req.files[0].originalname}`);
+            res.status(200).json({ message: "Arquivo Enviado!" })
+            return
+        })
+
+
+    } catch (err) {
+        console.log(err);
+    }
+})()
 
 server.listen(PORT, () => {
     console.log('Running on port: ', PORT);
