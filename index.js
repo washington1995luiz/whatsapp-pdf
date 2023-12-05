3973febb4620204d222185d6c878fb6e50d599e6
Washington <washington195luiz@gmail.com>
2023-12-04 23:51:01 -0300

    nova atualização

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const app = express();
 const WebSocket = require('ws');
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(helmet({
     contentSecurityPolicy: {
@@ -27,7 +27,6 @@ app.use(helmet({
 }));
 let connected = false;
 let initialized = false;
-let destroySession = false;
 let svgToSend = '';
 const server = require('http').createServer(app);
 const wss = new WebSocket.Server({ server: server })
@@ -46,7 +45,9 @@ wss.broadcast = function (data) {
             //authStrategy: new LocalAuth({ clientId: "WHATSAPP-PDF-Developer" })
         })
         if (initialized == false) {
-            client.initialize();
+            console.log('Iniciando sessão!');
+            client.initialize()
+            console.log('Sessão iniciada!');
             initialized = true;
         }
 
@@ -75,7 +76,7 @@ wss.broadcast = function (data) {
 
         client.on('qr', async qr => {
             try {
-                console.log("Trying generate new qr code")
+                console.log("QRCODE")
                 var svg_string = qrImage.imageSync(qr, { type: 'svg' });
                 svgToSend = svg_string;
                 wss.broadcast(svg_string);
@@ -94,11 +95,20 @@ wss.broadcast = function (data) {
         client.on('disconnected', async () => {
             console.log('Disconnected!');
             connected = false;
-            destroySession = true;
+            console.log('Destruindo sessão!');
+            client.destroy()
+            console.log('Sessão destruida!');
+            console.log('Iniciando sessão!');
+            client.initialize()
+            console.log('Sessão iniciada!');
+            wss.broadcast("disconnected")
 
         })
 
-        app.post("/sendMessage", upload.array("files"), async (req, res) => {
+        app.post("/sendFile", upload.array("files"), async (req, res) => {
+            if(req.files == undefined || req.files.length == 0){
+                return res.status(400).json({ error: "Nenhum arquivo encontrado!" })
+            }
             fs.renameSync(`${req.files[0].destination}${req.files[0].filename}`, `${req.files[0].destination}${req.files[0].originalname}`);
             const media = MessageMedia.fromFilePath(`${req.files[0].destination}${req.files[0].originalname}`);
             let number = req.body.number
@@ -110,6 +120,38 @@ wss.broadcast = function (data) {
             client.sendMessage(numberId._serialized, media);
             fs.rmSync(`${req.files[0].destination}${req.files[0].originalname}`);
             res.status(200).json({ message: "Arquivo Enviado!" })
+            return
+        })
+        app.post("/sendTextFile", upload.array("files"), async (req, res) => {
+            let { number, text } = req.body
+            if(req.files == undefined || req.files.length == 0 || text == '' || text == undefined){
+                return res.status(400).json({ error: "Nenhum arquivo ou texto encontrado!" })
+            }
+            fs.renameSync(`${req.files[0].destination}${req.files[0].filename}`, `${req.files[0].destination}${req.files[0].originalname}`);
+            const media = MessageMedia.fromFilePath(`${req.files[0].destination}${req.files[0].originalname}`);
+            let numberId = await client.getNumberId('55' + number);
+            if (numberId == null) {
+                res.status(400).json({ error: "Este não é um whatsapp válido.\nVerifique o número e tente novamente!" })
+                return
+            }
+            client.sendMessage(numberId._serialized, media, { caption: text });
+            fs.rmSync(`${req.files[0].destination}${req.files[0].originalname}`);
+            res.status(200).json({ message: "Arquivo e texto Enviado!" })
+            return
+        })
+        app.post("/sendText", upload.none(), async ( req, res ) => {
+            let { number, text } = req.body;
+            if(text == '' || text == undefined){
+                return res.status(400).json({ error: "Nenhum texto encontrado!" })
+            }
+
+            let numberId = await client.getNumberId('55' + number);
+            if (numberId == null) {
+                res.status(400).json({ error: "Este não é um whatsapp válido.\nVerifique o número e tente novamente!" })
+                return
+            }
+            client.sendMessage( numberId._serialized, text );
+            res.status(200).json({ message: "Texto Enviado!" })
             return
         })
 
